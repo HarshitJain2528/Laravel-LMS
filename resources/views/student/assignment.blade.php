9f2615d10721679febb921e2b7ccda93cd26f590
harbans-kaur <harbanskaur1602@gmail.com>
2024-01-24 17:33:50 +0530

    added files

--- resources/views/student/assignment.blade.php
+++ resources/views/student/assignment.blade.php
@@ -1,91 +1,93 @@
 @extends('student.layout.main')
-@section('student-assignment')
 
-    <div class="site-section-cover overlay" style="background-image: url('../student/images/hero_bg.jpg');">
+@section('student-assignment')
         @foreach($assignment as $title) 
-        <div class="container">
-            <div class="row align-items-center justify-content-center">
-                <div class="col-lg-10 text-center"> 
-                    <h1><strong >{{$title->assignment_title}}</strong></h1>
+            <div class="site-section-cover overlay" style="background-image: url('../student/images/hero_bg.jpg');">
+                <div class="container">
+                    <div class="row align-items-center justify-content-center">
+                        <div class="col-lg-10 text-center"> 
+                            <h1><strong >{{$title->assignment_title}}</strong></h1>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+
+            <div class="site-section">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12" style="border:1px solid black;padding:40px">
+                            <h1 class="heading" style="text-align: center;border-bottom:none">Assignment</h1>
+                            <p  style="text-align: right;">Total Marks: <span id="totalMarks">{{$title->total_marks}}</span></p>
+                            <p  style="text-align: right;">Obtained Marks: <span id="totalMarks">{{$title->marks->obtained_marks}}</span></p>
+                            {{-- <p>Created at: <span id="totalMarks">{{$title->created_at}}</span></p> --}}
+                            <div class="instructions">
+                                <h3>Instructions:</h3>
+                                <ul>
+                                    <li>All questions are compulsory.</li>
+                                    <li>Submit your assignment in pdf form of output only.</li>
+                                    <li>Marks will be given by teacher after submission.</li>
+                                    <!-- Add more instructions as needed -->
+                                </ul>
+                            </div>
+                            <div class="content-area">
+                                <h3>Question : </h3>
+                                @php
+                                    $questions = json_decode($title->assignment_question, true);
+                                @endphp
+                                @foreach ($questions as $questionKey => $question)
+                                    <p>Q: {{$question}}</p>
+                                @endforeach
+                            </div>
+                            <div class="submit-button" style="float:right">
+                                <button class="btn btn-primary" data-toggle="modal" data-target="#submitModal">Submit Assignment</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>        
+        @endforeach    
     </div>
-    <div class="site-section">
-      <div class="container">
-        <div class="row">
-          <div class="col-12" style="border:1px solid black;padding:40px">
-            <h1 class="heading" style="text-align: center;border-bottom:none">Assignment</h1>
-                <p  style="text-align: right;">Total Marks: <span id="totalMarks">{{$title->total_marks}}</span></p>
-                <p  style="text-align: right;">Obtained Marks: <span id="totalMarks">{{$title->marks->obtained_marks}}</span></p>
-                {{-- <p>Created at: <span id="totalMarks">{{$title->created_at}}</span></p> --}}
-                <div class="instructions">
-                    <h3>Instructions:</h3>
-                    <ul>
-                        <li>All questions are compulsory.</li>
-                        <li>Submit your assignment in pdf form of output only.</li>
-                        <li>Marks will be given by teacher after submission.</li>
-                        <!-- Add more instructions as needed -->
-                    </ul>
-                </div>
-                <div class="content-area">
-                    <h3>Question : </h3>
-                    @php
-                        $questions = json_decode($title->assignment_question, true);
-                    @endphp
-                    @foreach ($questions as $questionKey => $question)
-                        <p>Q: {{$question}}</p>
-                    @endforeach
-                </div>
-                <div class="submit-button" style="float:right">
-                    <button class="btn btn-primary" data-toggle="modal" data-target="#submitModal">Submit Assignment</button>
-                </div>
-            </div>
-         </div>
-        </div>
-      </div> 
-    </div> 
-    @endforeach
     <!-- Modal -->
-<div class="modal fade" id="submitModal" tabindex="-1" role="dialog" aria-labelledby="submitModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="submitModalLabel">Submit Assignment</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <!-- Form for user input -->
-                <form action="{{route('submitAssignment')}}" method="post" enctype="multipart/form-data">
-                    @csrf
-                    <div class="form-group">
-                        <label for="assignmentName">Name : </label>
-                        <input type="text" class="form-control" id="assignmentName" name="assignmentName" value="{{$title->assignment_title}}" readonly>
-                    </div>
-                    <div class="form-group">
-                        {{-- <label for="assignmentId">id : </label> --}}
-                        <input type="hidden" class="form-control" id="assignmentId" name="assignmentId" value="{{$title->id}}" readonly>
-                    </div>
-                    <div class="form-group">
-                        <label for="course">Course</label>
-                        <input type="text" class="form-control" id="course" name="course" value="{{ $title->course->course_title }}" readonly>
-                    </div>
-                    <div class="form-group">
-                        <label for="totalmarks">Total Marks : </label>
-                        <input type="text" class="form-control" id="totalmarks" name="totalmarks" value="{{$title->total_marks}}" readonly>
-                    </div>
-                    <div class="form-group">
-                        <label for="fileUpload">File Upload</label>
-                        <input type="file" class="form-control-file" id="fileUpload" name="fileUpload" required>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
+    <div class="modal fade" id="submitModal" tabindex="-1" role="dialog" aria-labelledby="submitModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="submitModalLabel">Submit Assignment</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <!-- Form for user input -->
+                    <form action="{{route('submitAssignment')}}" method="post" enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group">
+                            <label for="assignmentName">Name : </label>
+                            <input type="text" class="form-control" id="assignmentName" name="assignmentName" value="{{$title->assignment_title}}" readonly>
+                        </div>
+                        <div class="form-group">
+                            {{-- <label for="assignmentId">id : </label> --}}
+                            <input type="hidden" class="form-control" id="assignmentId" name="assignmentId" value="{{$title->id}}" readonly>
+                        </div>
+                        <div class="form-group">
+                            <label for="course">Course</label>
+                            <input type="text" class="form-control" id="course" name="course" value="{{ $title->course->course_title }}" readonly>
+                        </div>
+                        <div class="form-group">
+                            <label for="totalmarks">Total Marks : </label>
+                            <input type="text" class="form-control" id="totalmarks" name="totalmarks" value="{{$title->total_marks}}" readonly>
+                        </div>
+                        <div class="form-group">
+                            <label for="fileUpload">File Upload</label>
+                            <input type="file" class="form-control-file" id="fileUpload" name="fileUpload" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
+
     <hr>
 @endsection
     
--- resources/views/student/desc.blade.php
+++ resources/views/student/desc.blade.php
@@ -1,40 +1,38 @@
 @extends('student.layout.main')
 
 @section('student-description')
-<div class="site-section-cover overlay" style="background-image: url('../student/images/hero_bg.jpg');">
-       @foreach($data as $title) 
-        <div class="container">
-          <div class="row align-items-center justify-content-center">
-            <div class="col-lg-10 text-center">
-              <h1><strong >{{$title->topic}}</strong></h1>
-              {{-- <div class="pb-4 get"><strong class="text-white">Posted on May 22, 2020 &bullet; By Admin</strong></div> --}}
+    @foreach($data as $title) 
+        <div class="site-section-cover overlay" style="background-image: url('../student/images/hero_bg.jpg');">
+            <div class="container">
+                <div class="row align-items-center justify-content-center">
+                    <div class="col-lg-10 text-center">
+                        <h1><strong >{{$title->topic}}</strong></h1>
+                        {{-- <div class="pb-4 get"><strong class="text-white">Posted on May 22, 2020 &bullet; By Admin</strong></div> --}}
+                    </div>
+                </div>
+            </div>        
+        </div>
+
+        <div class="site-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h2 class="heading">Topic Content</h2>
+                        <br>
+                        @php
+                            $videoLinks = json_decode($title->video, true);
+                        @endphp
+                        @foreach ($videoLinks as $videoKey => $videoLink)
+                            <iframe width="1000" height="500" src="{{$videoLink}}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" style="margin:30px;"allowfullscreen ></iframe>
+                        @endforeach
+                    </div>
+                </div>
             </div>
-          </div>
+            <a href="{{url('next/'.$title->id)}}" class="btn btn-primary next">View Notes</a>            
         </div>
-      
-      </div>
-  
-    <div class="site-section">
-      <div class="container">
-        <div class="row">
-          <div class="col-12">
-            <h2 class="heading">Topic Content</h2>
-            <br>
-            @php
-                $videoLinks = json_decode($title->video, true);
-            @endphp
-            @foreach ($videoLinks as $videoKey => $videoLink)
-            <iframe width="1000" height="500" src="{{$videoLink}}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" style="margin:30px;"allowfullscreen ></iframe>
-            @endforeach
-          </div>
-        </div>
-      </div>
-      <a href="{{url('next/'.$title->id)}}" class="btn btn-primary next">View Notes</a>
-      
-    </div>
     @endforeach
-      
+    
     <hr>
-   @endsection
+@endsection
     
   
